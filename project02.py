7b8b544b1f3b85ad2f02d6f4ed8db1b69dd09322
avi050410-del <avi050410@gmail.com>
2026-06-08 19:08:30 +0300

    Update project02.py

--- project02.py
+++ project02.py
@@ -11,7 +11,6 @@ st.set_page_config(layout="wide", page_title="דשבורד מנהלת מרה\"ס
 def get_data():
     creds_dict = json.loads(st.secrets["gspread"]["credentials"])
     gc = gspread.service_account_from_dict(creds_dict)
-    
     sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1G-YKpuL3bBzesW0iEORf-x0WxvyeAM4At5RePg_OTKA/edit')
     worksheet = sh.worksheet('גיליון1')
     rows = worksheet.get_all_values()
@@ -26,7 +25,25 @@ def get_data():
     
     return df
 
-df = get_data()
+# כפתור ריענון
+if st.sidebar.button("🔄 רענן נתונים"):
+    st.cache_data.clear()
+    st.rerun()
+
+df = df_orig = get_data()
+
+# עיצוב כותרת
+st.markdown("<h1 style='text-align: center; color: #2E86C1;'>📊 דשבורד ניהול יועצים - מנהלת מרה\"ס</h1>", unsafe_allow_html=True)
+
+# דשבורד תכנון מול ביצוע לפי תחום
+st.subheader("📈 תכנון מול ביצוע לפי תחום")
+df_grouped = df.groupby('תחום')[['מסגרת שעות', 'ניצול']].sum()
+st.bar_chart(df_grouped)
+
+# סינון
+selected_tkhum = st.sidebar.selectbox("🔍 בחר תחום:", ["הכל"] + list(df['תחום'].unique()))
+if selected_tkhum != "הכל":
+    df = df[df['תחום'] == selected_tkhum]
 
 # חישוב המלצה
 today = datetime.datetime.now()
@@ -38,18 +55,10 @@ def calculate_recommendation(row):
 
 df['המלצה'] = df.apply(calculate_recommendation, axis=1)
 
-# כותרת וסינון
-st.markdown("<h1>📊 דשבורד ניהול יועצים - מנהלת מרה\"ס</h1>", unsafe_allow_html=True)
-selected_tkhum = st.sidebar.selectbox("🔍 בחר תחום:", ["הכל"] + list(df['תחום'].unique()))
-if selected_tkhum != "הכל":
-    df = df[df['תחום'] == selected_tkhum]
-
-# בחירת העמודות לפי הסדר שביקשת + הוספת ההמלצה
-cols_to_show = ["תחום", "שם יועץ", "תאריך סיום הזמנה", "מסגרת שעות", "ניצול", "יתרה", "אחוז ניצול", "המלצה"]
-df_display = df[cols_to_show].copy()
-
-# שינוי שמות העמודות לעברית יפה
+# הכנה לתצוגה
+df_display = df[["תחום", "שם יועץ", "תאריך סיום הזמנה", "מסגרת שעות", "ניצול", "יתרה", "אחוז ניצול", "המלצה"]].copy()
+df_display['תאריך סיום הזמנה'] = df_display['תאריך סיום הזמנה'].dt.strftime('%m/%Y')
 df_display.columns = ["תחום", "שם יועץ", "תאריך סיום", "מסגרת שעות", "ניצול", "יתרה", "אחוז ניצול", "המלצה"]
 
-# הצגת הטבלה
+st.subheader("📋 פירוט יועצים")
 st.dataframe(df_display, use_container_width=True)
